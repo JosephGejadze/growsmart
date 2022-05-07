43386e7cdc0e3a3916bb76d4aa52a6021b061228
ekachemia <eka.chemia@gmail.com>
2022-05-07 17:05:43 +0400

    fix image swappin issue

--- src/pages/homePage/images.js
+++ src/pages/homePage/images.js
@@ -1,24 +1,30 @@
 import growBox from "../../images/grow-box.png";
 import img3 from "../../images/img3.png";
-import { useState, useEffect } from "react";
+import chanto from "../../images/Niko.png";
+import { useState, useEffect, useCallback } from "react";
 import React from "react";
 
-const images = [growBox, img3];
+const images = [growBox, img3, chanto];
 
 export default function ImageSwapper() {
-  const [currentImage, setCurrentImage] = useState(null);
+  const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+    const timeoutId = setTimeout(() => {
+      if (currentImage >= images.length - 1) {
+        setCurrentImage(0);
+      } else {
+        setCurrentImage((x) => x + 1);
+      }
+      console.log(currentImage);
     }, 2000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+    return () => clearTimeout(timeoutId);
+  }, [currentImage]);
 
   return (
     <div>
-      <img width={500} height={500} src={currentImage} alt="growbox" />
+      <img width={500} height={500} src={images[currentImage]} alt="growbox" />
     </div>
   );
 }
